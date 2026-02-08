a714b86ba403841a0cf4dc33ad0727c1367e057b
bhuvi-2026 <bhuvanas2400@gmai.com>
2026-02-08 20:56:54 +0530

    1st error clean commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent, // ✅ declare header
+    HomeComponent    // ✅ declare home
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
