b5856e7396b02b700ca8837ad5d2cc84391d39ca
bhuvi-2026 <bhuvanas2400@gmai.com>
2026-02-16 12:28:46 +0530

    16-Feb-2026

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  cities = ['Bengaluru'];
+  selectedLocation = 'Bengaluru';
+  showLocationDropdown = false;
 
+  constructor(private elementRef: ElementRef) {}
+
+  openLocationDropdown() {
+    this.showLocationDropdown = true;
+  }
+
+  onLocationInput(event: Event) {
+    const val = (event.target as HTMLInputElement).value;
+    this.selectedLocation = val;
+    this.showLocationDropdown = true; // keep suggestions visible while typing
+  }
+
+  selectCity(city: string) {
+    this.selectedLocation = city;
+    this.showLocationDropdown = false;
+  }
+  closeLocationDropdown() {
+    this.showLocationDropdown = false;
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: Event) {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.showLocationDropdown = false;
+    }
+  }
 }
